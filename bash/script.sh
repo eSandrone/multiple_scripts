eb62afa6d360a06daddc8eb384330a1e5b2956f5
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2023-04-06 11:20:02 +0200

    Update script.sh

--- bash/script.sh
+++ bash/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 15
-pgrep -x "python ./python/wait.py"
+pgrep -l "./python/wait.py"
 echo "$?"
 if pgrep "python ./python/wait.py"; then python ../python/message.py; else echo "Tests concluded"; fi
