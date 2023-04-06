87a52633f3b11c1da0dd1e2b2346123065ee3123
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2023-04-06 12:52:02 +0200

    Update message.py

--- python/message.py
+++ python/message.py
@@ -2,6 +2,8 @@ import time
 import subprocess
 
 print("Sending message to teams")
-comando = "ps aux"
+comando = "touch file.txt"
 process = subprocess.Popen(comando.split(), shell=True)
+comando = "touch file_2.txt"
+process = subprocess.run(comando.split(), shell=True)
 #output, error = process.communicate()
