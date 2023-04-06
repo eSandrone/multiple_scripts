6593d4b8872d97fa711b30b3335bdf16cfc16cde
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2023-04-06 12:41:19 +0200

    Update message.py

--- python/message.py
+++ python/message.py
@@ -3,5 +3,5 @@ import subprocess
 
 print("Sending message to teams")
 comando = "ps aux"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+process = subprocess.Popen(comando.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
