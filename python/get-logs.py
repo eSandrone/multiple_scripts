a297a22d6da9592f68c64831dffece0aa356b3e8
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2023-04-19 11:41:38 +0200

    Update and rename webhook.py to get-logs.py

---
+++ python/get-logs.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import json
+import zipfile
+import requests
+from requests.auth import HTTPBasicAuth
+
+github_token = "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"
+run_id = sys.argv[1]
+image = "2"
+
+auth = HTTPBasicAuth("eSandrone", github_token)
+
+headers = {
+    "Accept": "application/vnd.github+json"
+}
+
+url = "https://api.github.com/repos/eSandrone/multiple_scripts/actions/runs/{}/logs".format(run_id)
+
+response = requests.request(
+    "GET",
+    url,
+    headers=headers,
+    auth=auth
+)
+
+print(response.content)
+
+open('./logs/logs.zip', 'wb').write(response.content)
