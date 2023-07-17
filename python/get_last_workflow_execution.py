82318d190e90f38a7159d819ac8f85b8296fdce3
Emanuele Sandrone <107463865+eSandrone@users.noreply.github.com>
2023-07-17 10:07:39 +0200

    Create get_last_workflow_execution.py

---
+++ python/get_last_workflow_execution.py
@@ -0,0 +1,31 @@
+import sys
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+
+# CONSTANTS
+GITHUB_USER = 'qxz30ra'
+
+# VARIABLES
+now = datetime.now().strftime("%H:%M:%S")
+github_token = sys.argv[1]
+workflow_id = sys.argv[2] if len(sys.argv) > 2 else 12885
+url = 'https://atc-github.azure.cloud.bmw/api/v3/repos/Service-and-Repairs/pip/actions/workflows/{}/runs'.format(workflow_id)
+
+
+# GITHUB CALL
+auth = HTTPBasicAuth(GITHUB_USER, github_token)
+headers = {
+    "Accept": "application/vnd.github+json"
+}
+response_json = requests.get(url, auth=auth, headers=headers).json()
+sys.stdout.write(json.dumps(response_json['workflow_runs'][0], indent=4))
+data = response_json['workflow_runs'][0]['run_started_at']
+run_started_at = data.replace('T', '').replace('Z', '')
+then = datetime.strptime(run_started_at, "%Y-%m-%d%H:%M:%S")
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%Y-%m-%d%H:%M:%S")
+parsed_datetime = datetime.strptime(formatted_datetime, "%Y-%m-%d%H:%M:%S")
+to_execute = "true" if (parsed_datetime - then).total_seconds()/3600 > 23 else "false"
+sys.stdout.write(to_execute)
